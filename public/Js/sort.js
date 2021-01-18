aea2b54c817d8f9d50025725e5f9fca654d8a8ca
gabrielafel <gabriela17042@gmail.com>
2021-01-18 03:11:46 -0800

    update

--- public/Js/sort.js
+++ public/Js/sort.js
@@ -1,3 +1,38 @@
+// function myFunction() {
+//   var input, filter, table, tr, td, i, txtValue;
+//   input = document.getElementById("myInput");
+//   filter = input.value.toUpperCase();
+//   table = document.getElementById("myTable");
+//   tr = table.getElementsByTagName("tr");
+//   for (i = 0; i < tr.length; i++) {
+//     td = tr[i].getElementsByTagName("td")[10];
+//     td = tr[i].getElementsByTagName("td")[12];
+//     if (td) {
+//       txtValue = td.textContent || td.innerText;
+//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//         tr[i].style.display = "";
+//       } else {
+//         tr[i].style.display = "none";
+//       }
+//     }       
+//   }
+// }
+
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+// $(document).ready(function() {
+//   $('#myTable').DataTable( {
+//       "pagingType": "full_numbers"
+//    });
+// });
+
 function sortTable(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("myTable");
--- routes/Data.js
+++ routes/Data.js
@@ -9,12 +9,13 @@ request.get("http://103.76.204.66:55557/WISECON_API/Service1.svc/LogConBranch", 
 });
 
 router.get("/", (req, res) =>{
-    const limit = 5;
+    const limit = req.query.limit || 5;
+    const filter = req.query.filter || null;
     var page = req.query.page || 1
     res.render("pages/table", {data: dataApi.slice((page-1)*limit, page*limit),
         limit: limit,
         current: page,
         pages: Math.ceil(Object.keys(dataApi).length/ limit)});
 });
-
 module.exports = router;
+//data: dataApi.filter(function(item) {return item.name == filter}).slice((page-1)*limit, page*limit)
